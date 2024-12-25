fcc49cf11f7ee2c0273e2693ce3bb92a26649425
Sushma Sarkar <123677590+SushmaSarkar26@users.noreply.github.com>
2024-12-25 12:02:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname,'../client/build')));
 //STATIC ROUTES
 app.get("*", function (Request, res){
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-})
+});
 
 mongoose.connect(process.env.mongooseURL)
     .then(() => {
